e575f0aa0b12b6431ad5a321eede78eb808bf7e5
Emma Nielsen <Emmanielsen843@gmail.com>
2023-09-15 13:39:56 +0200

    Getting id out in buy tickets

--- src/Components/Pages/BuyPage/BuyPage.jsx
+++ src/Components/Pages/BuyPage/BuyPage.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
-import { Layout } from '../../Layout/Layout'
+import React, { useEffect, useState } from "react";
+import { Layout } from "../../Layout/Layout";
+import axios from "axios";
+import { formatDate } from "../../Helpers/Helpers";
+import { useParams } from "react-router-dom";
 
 const BuyPage = () => {
-  return (
-    <Layout title="Køb billet">
-    <div>Buy your ticket here</div>
+  const [getTicket, setGetTicket] = useState({});
+  const { event_id } = useParams();
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const result = await axios.get(
+          `http://localhost:4000/events/${event_id}`
+        );
+        setGetTicket(result.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getData();
+  }, [setGetTicket, event_id]);
+
+  return(
+    <Layout>
+      {getTicket ? (
+        <>
+          <h2>{getTicket.title}</h2>
+          <div>
+            {getTicket.stage ? <p>{getTicket.stage.name}</p> : null}
+            <p>{formatDate(getTicket.startdate, true)}</p>
+          </div>
+        </>
+      ) : null}
     </Layout>
   )
-}
-
-export default BuyPage
+};
+export default BuyPage;
